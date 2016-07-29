0363f169448cdd98753edcc5181857b558d74125
smwood0906 <smarntson@gmail.com>
2016-07-28 20:27:09 -0700

    Add Categories

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-
+from .models import Post, Category
 # Register your models here.
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
 
--- blog/models.py
+++ blog/models.py
@@ -1,17 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 def post_upload_handler(instance, filename):
-    return"{slug}/img/{file}".format(slug=instance.slug, file=filename)
+    return "{slug}/img/{file}".format(slug=instance.slug, file=filename)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    slug = models.SlugField(unique=True)
+    desc = models.TextField(null=True, blank=True)
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
 
 class Post(models.Model):
     content = models.TextField()
-    date_created = models.DateTimeField(auto_now= True)
-    title = models.CharField(max_length= 300)
+    date_created = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=300)
     author = models.ForeignKey(User)
     slug = models.SlugField(unique=True)
-    img = models.ImageField(upload_to= post_upload_handler)
+    category = models.ForeignKey(Category)
+    img = models.ImageField(upload_to=post_upload_handler)
+
     def __str__(self):
         return self.title
 
